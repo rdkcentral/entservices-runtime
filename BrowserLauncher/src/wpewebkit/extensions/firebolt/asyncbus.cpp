673d1509b992676a3780c2830786d78482329b9d
Sathishkumar Deena Kirupakaran <satlead@gmail.com>
2026-05-28 22:49:38 -0400

    fix: compiler issues

--- BrowserLauncher/src/wpewebkit/extensions/firebolt/asyncbus.cpp
+++ BrowserLauncher/src/wpewebkit/extensions/firebolt/asyncbus.cpp
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- #include "AsyncBus.h"
+ #include "asyncbus.h"
 
 struct EmitData {
     AsyncBus* bus;
