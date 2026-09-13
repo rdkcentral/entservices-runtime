69cea18e348cff78fb3b54665d2b6d5c4de7c782
Sathishkumar Deena Kirupakaran <satlead@gmail.com>
2026-09-13 15:33:27 -0400

    feat: Add debugging support

--- BrowserLauncher/src/wpewebkit/wpewebkitview.cpp
+++ BrowserLauncher/src/wpewebkit/wpewebkitview.cpp
@@ -1337,6 +1337,7 @@ void WpeWebKitView::initWebExtensionsCallback(WebKitWebContext *context,
         g_variant_builder_init(&fireboltBuilder, G_VARIANT_TYPE("a{sv}"));
         g_variant_builder_add(&fireboltBuilder, "{sv}", "fireboltEndpoint",  g_variant_new_string(self->m_config->fireboltEndpoint().c_str()));
         g_variant_builder_add(&fireboltBuilder, "{sv}", "fireboltExtensionPath", g_variant_new_string(self->m_config->fireboltExtensionPath().c_str()));
+        g_variant_builder_add(&fireboltBuilder, "{sv}", "enableDebug", g_variant_new_boolean(true));
         g_variant_builder_add(&builder, "{sv}", "firebolt", g_variant_builder_end(&fireboltBuilder));
     }
 
